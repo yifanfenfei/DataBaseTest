6c9da8a95d25b61f72fa06c9da446b3646827e69
yifanfenfei <jianfeng224@gmail.com>
2013-02-26 09:30:23 +0800

    Initial Commit

---
+++ DataBaseTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DataBaseTest
+//
+//  Created by sun jianfeng on 2/26/13.
+//  Copyright (c) 2013 sun jianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DataBaseTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DataBaseTest
+//
+//  Created by sun jianfeng on 2/26/13.
+//  Copyright (c) 2013 sun jianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
